488857d4bd16187951fc8acb51f612c45239c843
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-07-27 20:17:47 -0300

    add function to run a python file with arguments and return the output, standard error and exit code if it's != from 0

---
+++ functions/run_python.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+from functions import config
+
+def run_python(working_directory: str, file_path: str, args=[]) -> str:
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(abs_working_directory, file_path))
+
+    # check that file_path is within working_directory
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    # check that file_path exists
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    # check that file_path is python file
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    cmd = ["python3", abs_file_path] + args
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, timeout=config.TIMEOUT, cwd=abs_working_directory)
+        output = f"STDOUT: {result.stdout}\nSTDERR: {result.stderr}\n"
+        if result.returncode != 0:
+            output += f"Process exited with code {result.returncode}"
+        return output
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
