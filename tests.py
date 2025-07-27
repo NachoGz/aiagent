b53a1a899ae48f07c50b05fab2e170dbadc13420
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-07-27 20:18:33 -0300

    add simple tests for run_python function

--- tests.py
+++ tests.py
@@ -1,25 +1,33 @@
-from functions.get_files_info import get_files_info
+from functions.run_python import run_python
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory")
+    result = run_python("calculator", "main.py")
+    print("==================Test 1==================")
     print(result)
+    print("==================Test 1==================")
+
+    result = run_python("calculator", "main.py", ["3 + 5"])
+    print("==================Test 2==================")
+    print(result)
+    print("==================Test 2==================")
+
+    result = run_python("calculator", "tests.py")
+    print("==================Test 3==================")
+    print(result)
+    print("==================Test 3==================")
+
+    result = run_python("calculator", "../main.py")
+    print("==================Test 4==================")
+    print(result)
+    print("==================Test 4==================")
     print()
 
-    result = get_files_info("calculator", "pkg")
-    print("Result for pkg directory")
+    result = run_python("calculator", "nonexistent.py")
+    print("==================Test 5==================")
     print(result)
+    print("==================Test 5==================")
     print()
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for /bin directory")
-    print(result)
-    print()
-
-    result = get_files_info("calculator", "../")
-    print("Result for ../ directory")
-    print(result)
-    print()
 
 if __name__ == '__main__':
     test()
