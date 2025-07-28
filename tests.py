612c9e99bf8ff23e18a129ff2b42fd17b86484a8
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-07-28 00:06:03 -0300

    updated tests to new name run_python_file

--- tests.py
+++ tests.py
@@ -1,28 +1,28 @@
-from functions.run_python import run_python
+from functions.run_python_file import run_python_file
 
 def test():
-    result = run_python("calculator", "main.py")
+    result = run_python_file("calculator", "main.py")
     print("==================Test 1==================")
     print(result)
     print("==================Test 1==================")
 
-    result = run_python("calculator", "main.py", ["3 + 5"])
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
     print("==================Test 2==================")
     print(result)
     print("==================Test 2==================")
 
-    result = run_python("calculator", "tests.py")
+    result = run_python_file("calculator", "tests.py")
     print("==================Test 3==================")
     print(result)
     print("==================Test 3==================")
 
-    result = run_python("calculator", "../main.py")
+    result = run_python_file("calculator", "../main.py")
     print("==================Test 4==================")
     print(result)
     print("==================Test 4==================")
     print()
 
-    result = run_python("calculator", "nonexistent.py")
+    result = run_python_file("calculator", "nonexistent.py")
     print("==================Test 5==================")
     print(result)
     print("==================Test 5==================")
